5cbdc9cde3b318f128625dfc3d12880d49710322
DentistOmarAshraf <omar_ashraf@live.com>
2024-02-23 00:55:12 +0000

    user update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+DATA BASE STORAGE ENGINE
+"""
 import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -11,10 +13,12 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class DBstorage:
+    """Data Base Engine"""
     __engine = None
     __session = None
-    
+
     def __init__(self):
         """Engine Variable"""
         usr = os.getenv('HBNB_MYSQL_USER')
@@ -32,12 +36,13 @@ class DBstorage:
             metadata.drop_all(bind=DBstorage.__engine)
 
     def all(self, cls=None):
+        """Return Data from Database According to class"""
         data = {}
-        if cls == None:
-            classes = [State, City]
+        if cls is None:
+            classes = [State, City, User]
             q_obj = []
             for cl in classes:
-                q_obj +=  DBstorage.__session.query(cl).all()
+                q_obj += DBstorage.__session.query(cl).all()
         else:
             q_obj = DBstorage.__session.query(cls).all()
 
@@ -46,19 +51,23 @@ class DBstorage:
             data[key] = obj
 
         return data
-        
+
     def new(self, obj):
+        """Add object to data Base"""
         DBstorage.__session.add(obj)
 
     def save(self):
+        """save after adding"""
         DBstorage.__session.commit()
 
     def delete(self, obj=None):
-        if obj != None:
+        """Delete from data base"""
+        if obj is None:
             DBstorage.__session.delete(obj)
             DBstorage.__session.commit()
 
     def reload(self):
+        """Intial Reloading"""
         Base.metadata.create_all(DBstorage.__engine)
         Session = sessionmaker(bind=DBstorage.__engine, expire_on_commit=False)
         DBstorage.__session = scoped_session(Session)
