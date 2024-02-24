d3aa63dc22f2e4c91ead810433f62d236fc27429
Mostafa Mahmoud Lotfy <95524857+BlackOps99@users.noreply.github.com>
2024-02-24 16:10:22 +0200

    Add the missed base

--- models/review.py
+++ models/review.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    user = relationship("User", cascade="all,delete", back_populates="review")
-    place = relationship("Place", cascade="all,delete", back_populates="review")
+    user = relationship("User", cascade="all,delete",
+                        back_populates="review")
+    place = relationship("Place", cascade="all,delete",
+                         back_populates="review")
