515398f35bcaeb14177b7c88fa09fdff34fcedd6
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-11 16:26:33 +0000

    ----

--- rehab.sh
+++ rehab.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-rep="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tdefault_type text/html;\n\t}"
-del=""
-sed -i "s|${rep}|${del}|" /etc/nginx/sites-available/default 
+sed -i '/location \/hbnb_static {/,/}/d' /etc/nginx/sites-available/default
