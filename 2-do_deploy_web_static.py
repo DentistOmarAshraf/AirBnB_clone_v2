5e5ff3a251dd3a332f05e907b98d0030110b6f64
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-11 23:48:09 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,14 +33,18 @@ def do_deploy(archive_path=None):
     base_name = os.path.basename(archive_path)
     wout_exet = os.path.splitext(base_name)[0]
     put(archive_path, "/tmp/")
-    run(f"mkdir -p /data/web_static/releases/{wout_exet}")
-    run(f"tar -xzf /tmp/{base_name} -C /data/web_static/releases/{wout_exet}")
-    run(f"rm /tmp/{base_name}")
-    x = f"/data/web_static/releases/{wout_exet}/web_static/"
-    run(f"mv {x}/* /data/web_static/releases/{wout_exet}")
-    run(f"rm -fr {x}")
+    run("mkdir -p /data/web_static/releases/{}"
+        .format(wout_exet))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        .format(base_name, wout_exet))
+    run("rm /tmp/{}"
+        .format(base_name))
+    with cd("/data/web_static/releases/"):
+        run("mv {}/web_static/* {}/"
+            .format(wout_exet, wout_exet))
+        run("rm -rf {}/web_static"
+            .format(wout_exet))
     run("rm -fr /data/web_static/current")
-    symb_link = "/data/web_static/current"
-    run(f"ln -s /data/web_static/releases/{wout_exet} {symb_link}")
-    run("sudo service nginx reload")
-    print("New version deployed!")
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(wout_exet))
+    print
