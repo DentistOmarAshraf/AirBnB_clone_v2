4813c66bb0df6bf8f555df0f69503df0c31f2be3
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-22 13:39:06 +0000

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Flask Project
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def slash(text):
+    return f"C {text.replace('_',' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def slash_2(text=None):
+    if not text:
+        return "Python is cool"
+    return f"Python {text.replace('_',' ')}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def is_num(n=None):
+    if int(n):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
